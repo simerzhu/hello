b1bd57b51b0c1e6810b21054d27b8127261ab9a2
simerzhu <simerzhu@163.com>
2019-04-15 21:49:59 +0800

    message

---
+++ src/main/java/com/simer/ImookApplication.java
@@ -0,0 +1,13 @@
+package com.simer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImookApplication.class, args);
+	}
+
+}
